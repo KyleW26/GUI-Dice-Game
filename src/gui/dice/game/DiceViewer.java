22fdac12b34e4307a429476ee1523f1db34a9481
KyleW26 <33125320+KyleW26@users.noreply.github.com>
2017-10-30 14:47:46 +0000

    Initial Commit
    Create a basic JFrame which is 600x300, set the title and set the default close operation.

---
+++ src/gui/dice/game/DiceViewer.java
@@ -0,0 +1,22 @@
+/*
+*   Author: Kyle Williams (eeu8e1)
+*   Date: 30th October 2017
+*   Assignment 2 - Create a simple GUI Dice Game
+*/
+package gui.dice.game;
+import java.util.*;
+import javax.swing.*;
+
+public class DiceViewer 
+{
+    public static void main(String[] args) 
+    {
+        JFrame frame = new JFrame();
+        frame.setSize(600,300);
+        frame.setTitle("Dice Viewer");
+        frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+        
+        frame.setVisible(true);
+    }
+    
+}
